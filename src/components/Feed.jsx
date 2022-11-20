4323530d0842947af2fbaba8cfefecd48953d7d6
ramishdev <rhmi1243@gmail.com>
2022-11-20 11:02:26 +0500

    Finished

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,24 +1,34 @@
 import {useState,useEffect} from 'react'
 import { Box,Stack,Typography } from '@mui/material'
 import {SideBar,Videos} from './'
-
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const Feed = () => {
+  const [selectedCategory, setselectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => {setVideos(data?.items)})   
+  }, [selectedCategory])
+  
   return (
     <Stack sx={{flexDirection :{sx: "column",md:"row"}}}>
-      <Box sx ={{height:{sx:'auto',md:'92vh'},borderRight:'1px solid #3d3d3d' , px: {sx:0,md:2}}}>
-        <SideBar/>
+      <Box sx ={{height:{sx:'auto',md:'86vh'},borderRight:'1px solid #3d3d3d' , px: {sx:0,md:2}}}>
+        <SideBar
+          selectedCategory={selectedCategory}
+          setselectedCategory={setselectedCategory}
+        />
         <Typography className='copyright' 
-        variant="body2" sx={{mt:1.5,color:"#fff"}}>
+          variant="body2" sx={{mt:4,color:"#fff"}}>
           Copyright 2022 Dark
         </Typography>
       </Box>
-      <Box p = {2} sx ={{overflowY: 'auto',height:'90vh',flex : 2}}>
+      <Box p = {2} sx ={{overflowY: 'auto',height:{sm:'auto',md:'86vh'},flex : 2}}>
         <Typography variant = "h4" fontWeight='bold' mb={2} sx={{color: 'white'}}>
-          New
-          <span style={{color: '#F31503'}}> video </span>  
+          {selectedCategory} 
+          <span style={{color: '#F31503'}}> videos  </span>  
         </Typography>
-        <Videos videos = {[]}/>
+        <Videos videos = {videos}/>
       </Box>
     </Stack>
   )
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {Stack,Box} from '@mui/material'
+import {VideoCard,ChannelCard} from './'
 
-const Videos = () => {
+const Videos = ({videos,direction}) => {
+  console.log(direction)
   return (
-    <div>Videos</div>
+    <Stack direction={direction ||'row'} flexWrap='wrap' justifyContent='start' gap={2}>
+      {videos && videos.map((video,idx) => (
+        <Box key={idx}>
+            {video.id.videoId && <VideoCard video={video}/>}
+            {video.id.channelId && <ChannelCard channelDetail={video}/>}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
-export default Videos
+export default React.memo(Videos);
